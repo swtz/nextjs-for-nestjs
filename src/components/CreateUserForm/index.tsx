a89d54699664807fb4839c88becbcabbdb9316b9
Leonardo Lüders <luderser@hotmail.com>
2025-07-05 13:22:08 -0300

    Class 263 — Created the CreateUserForm jsx elements.

--- src/components/CreateUserForm/index.tsx
+++ src/components/CreateUserForm/index.tsx
@@ -1,3 +1,60 @@
-export async function CreateUserForm() {
-  return <div>Hello, world!</div>;
+'use client';
+
+import { UserRoundIcon } from 'lucide-react';
+import { Button } from '../Button';
+import { InputText } from '../InputText';
+import Link from 'next/link';
+
+export function CreateUserForm() {
+  return (
+    <div>
+      <form action=''>
+        <InputText
+          type='text'
+          name='name'
+          labelText='Nome'
+          placeholder='Digite seu nome'
+          disabled={false}
+          defaultValue={''}
+          required
+        />
+        <InputText
+          type='email'
+          name='email'
+          labelText='E-mail'
+          placeholder='Digite seu e-mail'
+          disabled={false}
+          defaultValue={''}
+          required
+        />
+        <InputText
+          type='password'
+          name='password'
+          labelText='Senha'
+          placeholder='Digite sua senha'
+          disabled={false}
+          defaultValue={''}
+          required
+        />
+        <InputText
+          type='password'
+          name='password2'
+          labelText='Repetir senha'
+          placeholder='Digite sua de novo'
+          disabled={false}
+          defaultValue={''}
+          required
+        />
+
+        <Button disabled={false} type='submit'>
+          <UserRoundIcon />
+          Criar contra
+        </Button>
+
+        <p>
+          <Link href='/login'>Já tem conta? Entrar</Link>
+        </p>
+      </form>
+    </div>
+  );
 }
