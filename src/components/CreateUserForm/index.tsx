f1356afff87332472f18c0ba417ae9418d405264
Leonardo Lüders <luderser@hotmail.com>
2025-07-05 13:32:16 -0300

    Class 263 — Styled the CreateUserForm component.

--- src/components/CreateUserForm/index.tsx
+++ src/components/CreateUserForm/index.tsx
@@ -4,11 +4,17 @@ import { UserRoundIcon } from 'lucide-react';
 import { Button } from '../Button';
 import { InputText } from '../InputText';
 import Link from 'next/link';
+import clsx from 'clsx';
 
 export function CreateUserForm() {
   return (
-    <div>
-      <form action=''>
+    <div
+      className={clsx(
+        'flex items-center justify-center',
+        'text-center max-w-sm mt-16 mb-32 mx-auto',
+      )}
+    >
+      <form action='' className='flex-1 flex flex-col gap-6'>
         <InputText
           type='text'
           name='name'
@@ -46,12 +52,12 @@ export function CreateUserForm() {
           required
         />
 
-        <Button disabled={false} type='submit'>
+        <Button disabled={false} type='submit' className='mt-4'>
           <UserRoundIcon />
           Criar contra
         </Button>
 
-        <p>
+        <p className='text-sm/tight'>
           <Link href='/login'>Já tem conta? Entrar</Link>
         </p>
       </form>
