f6bcb24913315b58d6652646226072cf13873e49
Leonardo Lüders <luderser@hotmail.com>
2025-07-10 11:02:28 -0300

    Class 266 — Initialized the apiRequest<T>() function using the Fetch API and handled non-successful responses.

--- src/utils/api-request.ts
+++ src/utils/api-request.ts
@@ -11,3 +11,25 @@ type ApiRequestSuccess<T> = {
 };
 
 export type ApiRequest<T> = ApiRequestError | ApiRequestSuccess<T>;
+
+export async function apiRequest<T>(
+  url: string,
+  options?: RequestInit,
+): Promise<ApiRequest<T>> {
+  try {
+    const res = await fetch(url, options);
+    const json = await res.json().catch(() => null);
+
+    if (!res.ok) {
+      const errors = Array.isArray(json?.message)
+        ? json.message
+        : [json?.message || 'Erro inesperado'];
+
+      return {
+        errors,
+        success: false,
+        status: res.status,
+      };
+    }
+  } catch (err) {}
+}
