1a0fbbf3285870281406dd06d14e67d04ef48968
Leonardo Lüders <luderser@hotmail.com>
2025-07-10 10:46:14 -0300

    Class 266 — Created ApiRequest<T> type.

---
+++ src/utils/api-request.ts
@@ -0,0 +1,13 @@
+type ApiRequestError = {
+  errors: string[];
+  success: false;
+  status: number;
+};
+
+type ApiRequestSuccess<T> = {
+  data: T;
+  success: true;
+  status: number;
+};
+
+export type ApiRequest<T> = ApiRequestError | ApiRequestSuccess<T>;
