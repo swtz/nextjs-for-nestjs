fb7e79085109da93e27402a9e38b477502b54cd4
Leonardo Lüders <luderser@hotmail.com>
2025-08-04 22:13:05 -0300

    Class 268 — Removed all sign and verify JWT functions from the manage-login.ts file.

--- src/lib/login/manage-login.ts
+++ src/lib/login/manage-login.ts
@@ -1,19 +1,9 @@
 import bcrypt from 'bcryptjs';
 import { cookies } from 'next/headers';
-import { SignJWT, jwtVerify } from 'jose';
 import { redirect } from 'next/navigation';
 
-const jwtSecretKey = process.env.JWT_SECRET_KEY;
-const jwtEncodedKey = new TextEncoder().encode(jwtSecretKey);
-
 const loginExpSeconds = Number(process.env.LOGIN_EXPIRATION_SECONDS) || 86400;
-const loginExpStr = process.env.LOGIN_EXPIRATION_STRING || '1d';
 const loginCookieName = process.env.LOGIN_COOKIE_NAME || 'loginSession';
-
-type JwtPayload = {
-  username: string;
-  expiresAt: Date;
-};
 
 export async function hashPassword(password: string) {
   const hash = await bcrypt.hash(password, 10);
@@ -27,9 +17,9 @@ export async function verifyPassword(password: string, base64Hash: string) {
   return isValid;
 }
 
-export async function createLoginSession(username: string) {
+export async function createLoginSessionFromApi(jwt: string) {
   const expiresAt = new Date(Date.now() + loginExpSeconds * 1000);
-  const loginSession = await signJwt({ username, expiresAt });
+  const loginSession = jwt;
   const cookieStore = await cookies();
 
   cookieStore.set(loginCookieName, loginSession, {
@@ -40,54 +30,26 @@ export async function createLoginSession(username: string) {
   });
 }
 
-export async function deleteLoginSession() {
-  const cookieStore = await cookies();
-  cookieStore.set(loginCookieName, '', { expires: new Date(0) });
-  cookieStore.delete(loginCookieName);
-}
-
-export async function getLoginSession() {
+export async function getLoginSessionForApi() {
   const cookieSession = await cookies();
 
   const jwt = cookieSession.get(loginCookieName)?.value;
 
   if (!jwt) return false;
 
-  return verifyJwt(jwt);
+  return jwt;
 }
 
-export async function verifyLoginSession() {
-  const jwtPayload = await getLoginSession();
-
-  if (!jwtPayload) return false;
-
-  return jwtPayload?.username === process.env.LOGIN_USER;
-}
-
-export async function requireLoginSessionOrRedirect() {
-  const isAuthenticated = await verifyLoginSession();
+export async function requireLoginSessionOrRedirectForApi() {
+  const isAuthenticated = await getLoginSessionForApi();
 
   if (!isAuthenticated) {
-    redirect('/admin/login');
+    redirect('/login');
   }
 }
 
-export async function signJwt(jwtPayload: JwtPayload) {
-  return new SignJWT(jwtPayload)
-    .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
-    .setIssuedAt()
-    .setExpirationTime(loginExpStr)
-    .sign(jwtEncodedKey);
-}
-
-export async function verifyJwt(jwt: string | undefined = '') {
-  try {
-    const { payload } = await jwtVerify(jwt, jwtEncodedKey, {
-      algorithms: ['HS256'],
-    });
-    return payload;
-  } catch {
-    console.log('Invalid token');
-    return false;
-  }
+export async function deleteLoginSession() {
+  const cookieStore = await cookies();
+  cookieStore.set(loginCookieName, '', { expires: new Date(0) });
+  cookieStore.delete(loginCookieName);
 }
