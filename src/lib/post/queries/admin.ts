c5eb95b08deb7ff734c3fe9a5369b4d688d3b925
Leonardo Lüders <luderser@hotmail.com>
2025-08-09 11:00:01 -0300

    Class 271 — Created the new findPostByIdFromApiAdmin().

--- src/lib/post/queries/admin.ts
+++ src/lib/post/queries/admin.ts
@@ -1,8 +1,24 @@
+import { PostModelFromApi } from '@/models/post/post-model';
 import { postRepository } from '@/repositories/post';
+import { authenticatedApiRequest } from '@/utils/authenticated-api-request';
 import { cache } from 'react';
 
 export const findPostByIdAdmin = cache(async (id: string) => {
   return postRepository.findById(id);
+});
+
+export const findPostByIdFromApiAdmin = cache(async (id: string) => {
+  const postResponse = await authenticatedApiRequest<PostModelFromApi>(
+    `/post/me/${id}`,
+    {
+      headers: {
+        'Content-type': 'application/json',
+      },
+      cache: 'no-store',
+    },
+  );
+
+  return postResponse;
 });
 
 export const findAllPostsAdmin = cache(async () => {
