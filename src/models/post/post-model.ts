60bf6567c06847bb2a8cef98a98b257978ecb747
Leonardo Lüders <luderser@hotmail.com>
2025-08-09 10:50:43 -0300

    Class 271 — Created the new PostModelFromApi.

--- src/models/post/post-model.ts
+++ src/models/post/post-model.ts
@@ -1,3 +1,5 @@
+import { PublicUserDto } from '@/lib/user/schemas';
+
 export type PostModel = {
   id: string;
   title: string;
@@ -9,4 +11,17 @@ export type PostModel = {
   createdAt: string;
   updatedAt: string;
   author: string;
+};
+
+export type PostModelFromApi = {
+  id: string;
+  title: string;
+  slug: string;
+  excerpt: string;
+  content: string;
+  coverImageUrl: string;
+  published: boolean;
+  createdAt: string;
+  updatedAt: string;
+  author: PublicUserDto;
 };
